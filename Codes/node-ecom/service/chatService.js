6cbd87d2939809a24925098dff822bc424018e85
TejaswiniPydipally <143117469+TejaswiniPydipally@users.noreply.github.com>
2024-11-17 22:59:19 -0600

    Updated Chat Functionality

--- Codes/node-ecom/service/chatService.js
+++ Codes/node-ecom/service/chatService.js
@@ -1,15 +1,56 @@
 const Chat = require("../models/chat");
+const Notification = require("../models/notification");
  
 
-const addMessage = (params) => {
-    var model = new Chat()
-    model.message = params.msg  ;
-    model.userTo = params.message_to  ;   
-    model.userFrom = params.message_from  ;   
+
+
+const addNotification = (params) => {
+   
+    var model = new Notification()
+    model.message = `You have new messages from ${params.message_from_name} `  ;
+    model.user = params.message_to  ;   
+    model.type = 'chat'  ;   
+    
     model.save(function(err, data){
         if(err) console.log(err);
         
     })
+
+}
+
+
+const addMessage =(params) =>  new Promise(function(resolve, reject) {
+    // "Producing Code" (May take some time)
+    
+    var model = new Chat()
+    model.message = params.msg  ;
+    model.userTo = params.message_to  ;   
+    model.userFrom = params.message_from  ;   
+    model.messageReadFrom = true;   
+    model.messageReadTo = false;   
+    model.save(function(err, data){
+        if(err) console.log(err);
+        console.log('data', data)
+        resolve(data)
+    })
+    
+    addNotification(params)
+});
+
+const addMessage1 = (params) => {
+    var model = new Chat()
+    model.message = params.msg  ;
+    model.userTo = params.message_to  ;   
+    model.userFrom = params.message_from  ;   
+    model.messageReadFrom = true;   
+    model.messageReadTo = false;   
+    model.save(function(err, data){
+        if(err) console.log(err);
+        console.log('data', data)
+        return data;
+    })
+    
+    addNotification(params)
 
 }
 
